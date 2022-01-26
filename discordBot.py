5e1066c9e22249e5caa5ef43fb82bf71b49e6c7f
Ojas Srivastava <ojas1807@gmail.com>
2022-01-26 22:15:12 +0530

    Create discordBot.py

---
+++ discordBot.py
@@ -0,0 +1,24 @@
+import discord
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+client = discord.Client()
+
+my_bot = ChatBot(name="MyBot", 
+                read_only=True,
+                logic_adapters=["chatterbot.logic.MathematicalEvaluation", "chatterbot.logic.BestMatch"])
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
